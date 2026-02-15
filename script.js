aba9af11327ab486daee4e1209c5043d9606356a
PCM With Istiakh Sir <pcmwithistiakhsir@gmail.com>
2026-02-16 02:47:14 +0600

    Update script.js

--- script.js
+++ script.js
@@ -189,7 +189,7 @@
         const mImg = document.getElementById('mImg');
         
         function getReviewTextLimit() {
-            if (window.innerWidth < 600) return 250; 
+            if (window.innerWidth < 600) return 300; 
             return 300; 
         }
         
